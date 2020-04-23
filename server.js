69c706f97d6826c64110301ce3e5c8fa7171ed53
rerueter <rerueter@gmail.com>
2020-04-23 11:54:31 -0700

    *actual* initial server config

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+console.log("*** baby server reporting for duty ***");
+
+//______________________________________________________ Server Setup
 const express = require("express");
-const app = express;
-const PORT = 4000;
+const bParser = require("body-parser");
+
+//_________________________________________________ Instanced Modules
+const app = express();
+
+//___________________________________________________ Config Variabls
+
+const PORT = process.env.PORT || 4000;
 
 app.use("/", (req, res) => {
   res.send("hello world");
 });
 
 app.listen(PORT, () => {
-  console.log(`server listening at port ${PORT}`);
+  console.log(`***listening at port ${PORT}***`);
 });
