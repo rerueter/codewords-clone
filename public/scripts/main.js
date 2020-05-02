cbc0b8e38dcf02d608ac5c5e04a8a8a99b87e185
rerueter <rerueter@gmail.com>
2020-05-02 13:17:19 -0700

    fixes shuffler function, adds board reset, updates styling to include gameover animation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -30,7 +30,7 @@ const words = [
   "complicated",
 ];
 
-const deckBuild = (input) => {
+const deckBuilder = (input) => {
   let count = 0;
   for (let i = 0; i < input.length; i++) {
     const card = {};
@@ -45,25 +45,50 @@ const deckBuild = (input) => {
     card.number = i + 1;
     count++;
     state.cards.push(card);
-    console.log(card);
+    // console.log(card);
   }
+  console.log(state.cards);
 };
 
-const shuffler = () => {
-  const tempCards = [];
-  const cards = state.cards;
-  for (let i = 0; i < cards.length; i++) {
-    const removed = cards[Math.random(cards.length)];
+// this isn't overwriting its input as intended and I'm not sure why.
+// const shuffler = (input) => {
+//   const shuffled = [];
+//   while (input.length > 0) {
+//     let i = Math.floor(Math.random() * input.length, 1);
+//     shuffled.push(input[i]);
+//     input.splice(i, 1);
+//   }
+//   input = shuffled;
+//   console.log(input);
+// };
+
+const shuffler = (input) => {
+  const shuffled = [];
+  while (input.length > 0) {
+    let i = Math.floor(Math.random() * input.length, 1);
+    shuffled.push(input[i]);
+    input.splice(i, 1);
   }
+  for (let i = 0; i < shuffled.length; i++) {
+    input.push(shuffled[i]);
+  }
+  console.log(input);
 };
 
 const populater = () => {
+  wiper();
   for (let i = 0; i < state.cards.length; i++) {
     const { team, word } = state.cards[i];
     const div = document.createElement("div");
     div.classList.add("card", `${team}`);
     div.innerHTML = `<h3>${word}</h3>`;
     gameBoard.appendChild(div);
+  }
+};
+
+const wiper = () => {
+  while (gameBoard.firstChild) {
+    gameBoard.removeChild(gameBoard.lastChild);
   }
 };
 
@@ -81,6 +106,8 @@ const revealer = (event) => {
 gameBoard.addEventListener("click", revealer);
 
 //__________________________________________________Invoked Functions
-deckBuild(words);
+shuffler(words);
+deckBuilder(words);
+shuffler(state.cards);
 populater();
 // console.log(state.cards);
