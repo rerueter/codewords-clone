0ad124876e025c6fc59442f906d84eb106ffe0a6
rerueter <rerueter@gmail.com>
2020-05-04 14:46:33 -0700

    adds update route to Game collection

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -3,13 +3,19 @@ const router = express.Router();
 const DB = require("../models");
 
 //_____________________________________________________________ Index
-router.get("/", (req, res) => {
-  DB.Game.find({}, (err, foundGames) => {
-    if (err) {
-      return res.status(400).json({ msg: "something went wrong", err: err });
-    }
-    res.status(200).json({ msg: "index successful", foundGames });
-  });
+router.get("/", async (req, res) => {
+  try {
+    const foundGames = await DB.Game.find({});
+    const resObject = {
+      status: 200,
+      data: foundGames,
+      reqAt: new Date().toLocaleString(),
+    };
+    res.status(200).json(resObject);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ msg: "something went wrong", err });
+  }
 });
 
 //______________________________________________________________ Show
@@ -39,6 +45,27 @@ router.post("/", (req, res) => {
     res.status(200).json(responseObject);
   } catch (err) {
     return res.status(400).json({ msg: "something went wrong", err });
+  }
+});
+
+//____________________________________________________________ Update
+router.put("/:id", async (req, res) => {
+  try {
+    let foundGame = await DB.Game.findById(req.params.id);
+    foundGame.cards = req.body.cards;
+    foundGame.turn = req.body.turn;
+    foundGame.scoreA = req.body.scoreA;
+    foundGame.scoreB = req.body.scoreB;
+    foundGame.save();
+    const resObject = {
+      status: 200,
+      data: foundGame,
+      reqAt: new Date().toLocaleString(),
+    };
+    res.status(200).json(resObject);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "update failure", err });
   }
 });
 
